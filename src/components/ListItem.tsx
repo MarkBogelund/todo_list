894f51aec278dc51a3dd26f362713b1ae500b06c
Mark Myrtoft Bøgelund <mark.bogelund@boostplm.com>
2023-09-26 09:58:40 +0200

    Fixed icons

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,5 +1,6 @@
 import ClearIcon from "@mui/icons-material/Clear";
 import DoneIcon from "@mui/icons-material/Done";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function ListItem({
   completed,
@@ -18,21 +19,20 @@ function ListItem({
     <li
       className={`w-[80%] aspect-square flex justify-center items-center rounded-md ${
         completed ? "bg-[#3c6f69]" : "bg-[#96BBA2]"
-      } relative group font-light`}
+      } relative group font-light overflow-hidden`}
     >
       <div
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center
-        w-full h-full bg-black bg-opacity-50 rounded-md
-      invisible group-hover:visible"
+        className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 
+      invisible group-hover:visible flex items-center justify-center"
       >
         <button
           className="text-white w-8 h-8"
           onClick={() => toggleTodo?.(id, !completed)}
         >
-          <DoneIcon />
+          {completed ? <ClearIcon /> : <DoneIcon />}
         </button>
         <button className="text-white w-8 h-8" onClick={() => deleteTodo(id)}>
-          <ClearIcon />
+          <DeleteIcon />
         </button>
       </div>
       <h2 className="text-white text-center w-full">{title}</h2>
