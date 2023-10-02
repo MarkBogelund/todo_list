450177166fcf5409ab9590861317abdc05a9f14e
Mark Myrtoft Bøgelund <mark.bogelund@boostplm.com>
2023-10-02 19:14:37 +0200

    Updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
 import { colors } from "../App";
-import { Home } from "@mui/icons-material";
+import { Menu, Login } from "@mui/icons-material";
+import LogoutIcon from "@mui/icons-material/Logout";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import CreateIcon from "@mui/icons-material/Create";
 import { UserAuth } from "../context/AuthContext";
+import { useState, useRef, useEffect } from "react";
 
 function Navbar() {
   const { user, logout } = UserAuth();
+
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuRef = useRef<HTMLDivElement | null>(null);
 
   const handleLogout = async () => {
     try {
@@ -14,40 +21,66 @@ function Navbar() {
     }
   };
 
+  useEffect(() => {
+    function handleDocumentClick(event: MouseEvent) {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setOpenMenu(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleDocumentClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleDocumentClick);
+    };
+  }, []);
+
   return (
-    <div className="absolute top-4 w-full z-50 flex justify-between items-center">
-      <Link to="/">
-        <button
-          className={`bg-[${colors.cardNotDone}] p-2 ml-4 rounded-full text-white text-sm font-thin`}
+    <div className="absolute top-6 left-6 z-50 flex flex-col" ref={menuRef}>
+      <button
+        className={`bg-[${colors.cardNotDone}] rounded-full text-white text-sm font-thin w-12 aspect-square`}
+        onClick={() => setOpenMenu(!openMenu)}
+      >
+        <Menu />
+      </button>
+      {openMenu ? (
+        <div
+          className={`rounded-md my-2 bg-[${colors.cardNotDone}] flex-1 p-2`}
         >
-          <Home />
-        </button>
-      </Link>
-      {user ? (
-        <button
-          className={`bg-[${colors.cardNotDone}] p-2 mr-4 rounded-full text-white text-sm font-thin`}
-          onClick={handleLogout}
-        >
-          Log out
-        </button>
-      ) : (
-        <div className="flex justify-end items-center">
-          <Link to="/signup">
-            <button
-              className={`bg-[${colors.cardNotDone}] p-2 mr-4 rounded-full text-white text-sm font-thin`}
-            >
-              Sign up
-            </button>
-          </Link>
-          <Link to="/login">
-            <button
-              className={`bg-[${colors.cardNotDone}] p-2 mr-4 rounded-full text-white text-sm font-thin`}
-            >
-              Log in
-            </button>
-          </Link>
+          {user ? (
+            <ul className="flex flex-col justify-evenly items-start w-full h-full mx-4 gap-2">
+              <li className={`text-white text-sm font-thin rounded-md`}>
+                <PersonOutlineIcon className="mr-2" />
+                {user.email}
+              </li>
+              <Link to="/login">
+                <li
+                  className={`text-white text-sm font-thin rounded-md`}
+                  onClick={handleLogout}
+                >
+                  <LogoutIcon className="mr-2" />
+                  Log out
+                </li>
+              </Link>
+            </ul>
+          ) : (
+            <ul className="flex flex-col justify-evenly items-start w-full h-full mx-4">
+              <Link to="/login">
+                <li className={`text-white text-sm font-thin rounded-md`}>
+                  <Login className="mr-2" />
+                  Log in
+                </li>
+              </Link>
+              <Link to="/signup">
+                <li className={`text-white text-sm font-thin rounded-md`}>
+                  <CreateIcon className="mr-2" />
+                  Sign up
+                </li>
+              </Link>
+            </ul>
+          )}
         </div>
-      )}
+      ) : null}
     </div>
   );
 }
